480b87808ba541ad67ccc5d035e0d21215003337
imoctafirdaus-maker <imoctafirdaus@gmail.com>
2026-04-30 19:22:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,19 @@
-// Naikkan versi CACHE_NAME setiap kali kamu melakukan update pada index.html
-const CACHE_NAME = 'game-v3';
-
-// Daftarkan semua file yang ingin disimpan secara offline
+const CACHE_NAME = 'game-v4';
 const assets = [
-  '/',
+  './',
   './index.html',
   './manifest.json',
-  './1777546745740.png' // Pastikan nama file ini sama persis dengan yang ada di GitHub
+  './1777546745740.png'
 ];
 
-// Tahap Instalasi: Menyimpan aset ke dalam cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Mengamankan Aset ke Cache');
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
-// Tahap Aktivasi: Menghapus cache lama agar kode baru bisa jalan
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Menghapus Cache Lama');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Tahap Fetch: Mengambil data dari cache jika sedang offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
